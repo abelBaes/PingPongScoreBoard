a9af9a76c780502c7b9302923e0dd67bafac9bc2
abelBaes <abel.baes77@gmail.com>
2026-08-31 23:45:43 -0300

    feat(MainActivity): criando base de ui

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3039307/pingpongscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3039307/pingpongscoreboard/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.prdm.sc3039307.pingpongscoreboard.ui.theme.PingPongScoreBoardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PingPongScoreBoardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,82 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Placar de Ping Pong",
+            fontSize = 30.sp,
+            modifier = Modifier.padding(20.dp)
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TeamCard(
+                teamName = "Jogador A",
+                teamPoints = 0,
+                Modifier.weight(1f)
+            )
+            TeamCard(
+                teamName = "Jogador B",
+                teamPoints = 0,
+                Modifier.weight(1f)
+            )
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "Reiniciar partida",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+
+        }
+    }
+}
+
+@Composable
+fun TeamCard(teamName: String, teamPoints: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = teamName,
+            fontSize = 25.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "$teamPoints",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(10.dp)
+        ) {
+            Text(
+                text = "+ 1 Ponto",
+                fontSize = 15.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     PingPongScoreBoardTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
